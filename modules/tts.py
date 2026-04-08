97c10e36596831dfb330cf983122791d83247b08
Passos <passosmatheus.work@gmail.com>
2026-04-08 20:58:40 -0300

    fix: reseampleando.

--- modules/tts.py
+++ modules/tts.py
@@ -1,8 +1,11 @@
 import numpy as np
 import sherpa_onnx
 
+
+from math import gcd           
 from pathlib import Path
- 
+from scipy.signal import resample_poly                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+                         
 
 SPEED = 1.0
 
@@ -13,6 +16,18 @@ MODEL_DIR = str(BASE_DIR / "core/models/tts/vits-piper-pt_BR-faber-medium")
 MODEL_PATH = f"{MODEL_DIR}/pt_BR-faber-medium.onnx"
 TOKENS_PATH = f"{MODEL_DIR}/tokens.txt"
 DATA_DIR = f"{MODEL_DIR}/espeak-ng-data"
+                                                                                                                                                                                                                                                                                                              
+                                                                                                                                                                                                                                                                                                                                            
+def resample_audio(audio: np.ndarray, orig_sr: int, target_sr: int = 16000) -> np.ndarray:                                                                                                                                                                                                                                            
+    if orig_sr == target_sr:                                                                                                                                                                                                                                                                                                                  
+        return audio                                                                                                                                                                                                                                                                                                                          
+                                                                                                                                                                                                                                                                                                                                            
+    g = gcd(orig_sr, target_sr)                                                                                                                                                                                                                                                                                                               
+    up = target_sr // g                                                                                                                                                                                                                                                                                                                       
+    down = orig_sr // g                                                                                                                                                                                                                                                                                                                       
+                                                                                                                                                                                                                                                                                                                                            
+    resampled = resample_poly(audio, up, down)                                                                                                                                                                                                                                                                                                
+    return resampled.astype(audio.dtype)         
 
 
 def create_speech_synthesis_model():
@@ -33,7 +48,10 @@ def sintetize_speech_segment(sintetize_spech_model, speech_segment: str):
     audio = sintetize_spech_model.generate(
         speech_segment, speed=SPEED
     )
-    samples = np.array(audio.samples, dtype=np.float32)
     
-    return samples
+    samples = np.array(audio.samples, dtype=np.float32)    
+                                                                                                                                                                                                                                                        
+    resampled = resample_audio(samples, audio.sample_rate)                                                                                                                                                                                                                                                            
+                                                                                                                                                                                                                                                                                                                                                
+    return resampled                                                       
 
