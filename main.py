daea90eea9aa3b81e398b9b3fe35ded56e0bb454
Passos <passosmatheus.work@gmail.com>
2026-04-08 03:32:36 -0300

    feat: melhorando encerramento e adicionando modo echo pra dar pra testar o microfone e o alto falante.

--- main.py
+++ main.py
@@ -11,8 +11,19 @@ from modules.audio import capture_audio_input_and_produce_audio_output
 
 
 async def main():
-    await asyncio.wait_for(capture_audio_input_and_produce_audio_output(), 15)
-    print('esperou certo!')
+
+    capture_and_produce_audio = asyncio.create_task(
+            capture_audio_input_and_produce_audio_output(mode='echo')
+    )
+
+    try:
+        await capture_and_produce_audio
+    
+    finally:
+        print('finalizando tasks e threads pendentes')
+        capture_and_produce_audio.cancel()
+
+
 
 if __name__ == "__main__":
     asyncio.run(main())
--- modules/workers/audio_workers.py
+++ modules/workers/audio_workers.py
@@ -4,19 +4,28 @@ import asyncio
 def audio_consumer_worker(stop_flag, frames_amount, _input_stream,  _output_queue, loop):
 
     while not stop_flag.is_set():
-        
-        data = _input_stream.read(frames_amount)
-        
-        asyncio.run_coroutine_threadsafe(_output_queue.put(data), loop)
+        _fut = None
 
+        try: 
+            data = _input_stream.read(frames_amount)
+            _fut = asyncio.run_coroutine_threadsafe(_output_queue.put(data), loop)
+        
+        except OSError:
+            print('stream de input cancelado manualmente')
+            break            
 
 async def audio_producer_worker(stop_flag, _input_queue, output_stream):
     
     while not stop_flag.is_set():
+        try:        
+            audio_bytes = await _input_queue.get()
+            output_stream.write(audio_bytes)
+            
+        except OSError:
+            print('stream de output cancelado manualmente')
+            break
+            
+        except asyncio.CancelledError:
+            print('producer_worker cancelado manualmente.')
+            break 
         
-        audio_bytes = await _input_queue.get()
-        output_stream.write(audio_bytes)
-        
-        
-        
-
