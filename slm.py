fd1f0ba29e09c929f7e2d697db30b07bb60c8e4a
Passos <passosmatheus.work@gmail.com>
2026-04-09 19:21:13 -0300

    feat: testando trocas entre adapters LoRA. Ainda não há métodos 'high-level' expostos pelo biding python, então estou usando arrays e funções C puras chamando a interface em C direto.

---
+++ slm.py
@@ -0,0 +1,124 @@
+import ctypes 
+import _ctypes
+ 
+ 
+
+import llama_cpp
+from llama_cpp import llama_set_adapters_lora, llama_adapter_lora_init
+
+def create_user_message(message: str):
+    
+
+    return {'role': 'user', 'content': message + '/no_think'}
+
+
+slm = llama_cpp.Llama(
+    model_path='slm/Qwen3-0.6B-Q8_0.gguf',
+    n_ctx=2048,
+    verbose=False,
+)
+
+messages = [
+    {'role': 'system', 'content': 'Você é um assistente que só fala português brasileiro, siga a instruções corretamente.'}
+]
+
+messages.append(create_user_message('Diga exatamente "Teste."'))
+
+teste = slm.create_chat_completion(messages=messages, max_tokens=100, stream=False)
+print(f'teste do slm: {teste}')
+
+
+
+def active_adapter(ctx, target_adapter, name_pointer_tuple_list, adapters, scales):
+    _count = len(scales)
+    actived_adapter = None
+
+   
+
+    for i in range(0, _count):
+        if target_adapter == name_pointer_tuple_list[i][0]:
+            actived_adapter = target_adapter
+            scales[i] = 1.0
+
+            continue 
+        
+        scales[i] = 0.0
+
+    if active_adapter is None:
+        raise 'adapter target não ta na lista'
+
+    code = llama_set_adapters_lora(ctx, adapters, _count, scales)
+
+    if code != 0:
+        raise 'deu ruim tbm'
+
+    return actived_adapter, 1.0
+
+def _initialize_adapters(ctx, adapters):
+    _adapters_count = len(adapters)
+
+    _void_c_float_scales_array = _create_pure_C_array(ctypes.c_float, _adapters_count)
+    _void_c_adapters_pointer_array = _create_pure_C_array(type(adapters[-1][-1]), _adapters_count)
+
+
+    _scales_c_float_array = _void_c_float_scales_array(0.0 * _adapters_count)
+    _adapter_pointers_c_array = _void_c_adapters_pointer_array(*[adapter_p for _, adapter_p in adapters])
+
+    code = llama_set_adapters_lora(ctx, _adapter_pointers_c_array, _adapters_count, _scales_c_float_array)
+
+    if code != 0:
+        raise 'deu ruim'
+    
+    return _adapter_pointers_c_array, _scales_c_float_array 
+    
+
+
+def _load_adapter_in_memory(model, adapter_path, adapter_name):
+    pointer = llama_adapter_lora_init(model, adapter_path.encode('utf-8'))
+   
+    print(f'{adapter_name} carregado com sucesso em memória.')
+    print(pointer)
+
+    return pointer
+    
+def _create_pure_C_array(_type, size):
+    return _type * size
+   
+ 
+
+adapter_a = _load_adapter_in_memory(slm.model, 'adapter_v8/adapter_v8.gguf', 'planner')
+adapter_b = _load_adapter_in_memory(slm.model, 'adapter_b/adapter_b.gguf', 'cópia do planner 1')
+adapter_c = _load_adapter_in_memory(slm.model, 'adapter_c/adapter_c.gguf', 'cópia do planner 2')
+
+adapters_pointer_python_list = [
+    ('adapter_a', adapter_a), 
+    ('adapter_b', adapter_b), 
+    ('adapter_c', adapter_c)
+]
+
+adapters_pointers_c_array, scales_c_float_array =_initialize_adapters(slm.ctx, adapters_pointer_python_list)
+
+actived_adapter, current_actived_adapter_scale = active_adapter(slm.ctx, 'adapter_a', adapters_pointer_python_list, adapters_pointers_c_array, scales_c_float_array)
+print(f'adapter ativo: {actived_adapter}, scale: {current_actived_adapter_scale}')
+
+actived_adapter, current_actived_adapter_scale = active_adapter(slm.ctx, 'adapter_b', adapters_pointer_python_list, adapters_pointers_c_array, scales_c_float_array)
+print(f'adapter ativo: {actived_adapter}, scale: {current_actived_adapter_scale}')
+
+actived_adapter, current_actived_adapter_scale = active_adapter(slm.ctx, 'adapter_c', adapters_pointer_python_list, adapters_pointers_c_array, scales_c_float_array)
+print(f'adapter ativo: {actived_adapter}, scale: {current_actived_adapter_scale}')
+
+actived_adapter, current_actived_adapter_scale =active_adapter(slm.ctx, 'adapter_a', adapters_pointer_python_list, adapters_pointers_c_array, scales_c_float_array)
+print(f'adapter ativo: {actived_adapter}, scale: {current_actived_adapter_scale}')
+
+
+
+
+
+
+
+ 
+ 
+    
+
+ 
+
