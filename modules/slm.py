5679a3e39a2779c1debb702ffe4114046fb768bb
Passos <passosmatheus.work@gmail.com>
2026-04-09 22:40:37 -0300

    testando implementação básica (e bem feia) do fluxo completo de troca entre os agentes restaurante kv_cache, adapters e mensagens, funcionou.

--- modules/slm.py
+++ modules/slm.py
@@ -1,10 +1,11 @@
 import ctypes 
 import _ctypes
+from pathlib import Path
  
  
 
 import llama_cpp
-from llama_cpp import llama_set_adapters_lora, llama_adapter_lora_init
+from llama_cpp import llama_set_adapters_lora, llama_adapter_lora_init, LlamaState
 
 def create_user_message(message: str):
     
@@ -12,8 +13,14 @@ def create_user_message(message: str):
     return {'role': 'user', 'content': message + '/no_think'}
 
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+MODEL_DIR = str(BASE_DIR / "core/models/slm/qwen-3-0.6B")
+
+ADAPTERS_DIR = f"{MODEL_DIR}/lora_adapters"
+MODEL_PATH = f"{MODEL_DIR}/Qwen3-0.6B-Q8_0.gguf"
+
 slm = llama_cpp.Llama(
-    model_path='slm/Qwen3-0.6B-Q8_0.gguf',
+    model_path=MODEL_PATH,
     n_ctx=2048,
     verbose=False,
 )
@@ -28,8 +35,6 @@ messages = [
 def active_adapter(ctx, target_adapter, name_pointer_tuple_list, adapters, scales, personalized_scale = 1.0):
     _count = len(scales)
     actived_adapter = None
-
-   
 
     for i in range(0, _count):
         if target_adapter == name_pointer_tuple_list[i][0]:
@@ -66,8 +71,6 @@ def _initialize_adapters(ctx, adapters):
         raise 'deu ruim'
     
     return _adapter_pointers_c_array, _scales_c_float_array 
-    
-
 
 def _load_adapter_in_memory(model, adapter_path, adapter_name):
     pointer = llama_adapter_lora_init(model, adapter_path.encode('utf-8'))
@@ -82,9 +85,10 @@ def _create_pure_C_array(_type, size):
    
  
 
-adapter_a = _load_adapter_in_memory(slm.model, 'adapter_v8/adapter_v8.gguf', 'planner')
-adapter_b = _load_adapter_in_memory(slm.model, 'adapter_b/adapter_b.gguf', 'cópia do planner 1')
-adapter_c = _load_adapter_in_memory(slm.model, 'adapter_c/adapter_c.gguf', 'cópia do planner 2')
+adapter_a = _load_adapter_in_memory(slm.model, f'{ADAPTERS_DIR}/adapter_v8/adapter_v8.gguf', 'planner')
+adapter_b = _load_adapter_in_memory(slm.model, f'{ADAPTERS_DIR}/adapter_b/adapter_b.gguf', 'cópia do planner 1')
+adapter_c = _load_adapter_in_memory(slm.model, f'{ADAPTERS_DIR}/adapter_c/adapter_c.gguf', 'cópia do planner 2')
+
 
 adapters_pointer_python_list = [
     ('adapter_a', adapter_a), 
@@ -92,93 +96,95 @@ adapters_pointer_python_list = [
     ('adapter_c', adapter_c)
 ]
 
+
+_init_state = slm.save_state()
+
+kv_cache_adapters = {
+    'adapter_a': {
+        'state': _init_state,
+        'messages': [{'role': 'system', 'content': 'Você é um assistente que só fala português brasileiro, siga a instruções corretamente.'}]
+    },
+    'adapter_b': {
+        'state': _init_state,
+        'messages': [{'role': 'system', 'content': 'Você é um assistente que só fala português brasileiro, siga a instruções corretamente.'}]
+    },
+    'adapter_c': {
+        'state': _init_state,
+        'messages': [{'role': 'system', 'content': 'Você é um assistente que só fala português brasileiro, siga a instruções corretamente.'}]
+    },
+}
+
 adapters_pointers_c_array, scales_c_float_array =_initialize_adapters(slm.ctx, adapters_pointer_python_list)
 
-actived_adapter, current_actived_adapter_scale = active_adapter(slm.ctx, 'adapter_a', adapters_pointer_python_list, adapters_pointers_c_array, scales_c_float_array, personalized_scale=0.5)
+actived_adapter, current_actived_adapter_scale = active_adapter(slm.ctx, 'adapter_a', adapters_pointer_python_list, adapters_pointers_c_array, scales_c_float_array, personalized_scale=1.0)
 print(f'adapter ativo: {actived_adapter}, scale: {current_actived_adapter_scale}')
 
-messages.append(create_user_message('Preciso marcar uma call dez horas"'))
+kv_cache_adapters['adapter_a']['messages'].append(create_user_message('Qual a capital de minas gerais?'))
 
-test_inference_with_adapter = slm.create_chat_completion(messages=messages, max_tokens=100, stream=False)
+test_inference_with_adapter = slm.create_chat_completion(messages=kv_cache_adapters['adapter_a']['messages'], max_tokens=100, stream=False)
+
 print(f'teste da inferência com o {active_adapter}, com scale {current_actived_adapter_scale}, resultado:{test_inference_with_adapter}')
 
-messages.pop()
+kv_cache_adapters['adapter_a']['state'] = slm.save_state()
+
+print(_init_state.n_tokens)
+print(kv_cache_adapters['adapter_a']['state'].n_tokens)
+
 slm.reset()
-
-
-actived_adapter, current_actived_adapter_scale = active_adapter(slm.ctx, 'adapter_b', adapters_pointer_python_list, adapters_pointers_c_array, scales_c_float_array, personalized_scale=0.1)
-print(f'adapter ativo: {actived_adapter}, scale: {current_actived_adapter_scale}')
-
-messages.append(create_user_message('Preciso marcar uma call dez horas"'))
-
-test_inference_with_adapter = slm.create_chat_completion(messages=messages, max_tokens=100, stream=False)
-print(f'teste da inferência com o {active_adapter}, com scale {current_actived_adapter_scale}, resultado:{test_inference_with_adapter}')
-
-messages.pop()
-slm.reset()
-
-
-actived_adapter, current_actived_adapter_scale = active_adapter(slm.ctx, 'adapter_c', adapters_pointer_python_list, adapters_pointers_c_array, scales_c_float_array, personalized_scale=1.0)
-print(f'adapter ativo: {actived_adapter}, scale: {current_actived_adapter_scale}')
-
-messages.append(create_user_message('Preciso marcar uma call dez horas"'))
-
-test_inference_with_adapter = slm.create_chat_completion(messages=messages, max_tokens=100, stream=False)
-print(f'teste da inferência com o {active_adapter}, com scale {current_actived_adapter_scale}, resultado:{test_inference_with_adapter}')
-
-messages.pop()
-slm.reset()
-
-actived_adapter, current_actived_adapter_scale =active_adapter(slm.ctx, 'adapter_a', adapters_pointer_python_list, adapters_pointers_c_array, scales_c_float_array, personalized_scale=0.7)
-print(f'adapter ativo: {actived_adapter}, scale: {current_actived_adapter_scale}')
-
-messages.append(create_user_message('Preciso marcar uma call dez horas"'))
-
-test_inference_with_adapter = slm.create_chat_completion(messages=messages, max_tokens=100, stream=False)
-print(f'teste da inferência com o {active_adapter}, com scale {current_actived_adapter_scale}, resultado:{test_inference_with_adapter}')
-
-messages.pop()
-slm.reset()
-
-actived_adapter, current_actived_adapter_scale =active_adapter(slm.ctx, 'adapter_a', adapters_pointer_python_list, adapters_pointers_c_array, scales_c_float_array, personalized_scale=0.0)
-print(f'adapter ativo: {actived_adapter}, scale: {current_actived_adapter_scale}')
-
-messages.append(create_user_message('Marca uma call dez horas"'))
-
-test_inference_with_adapter = slm.create_chat_completion(messages=messages, max_tokens=100, stream=False)
-print(f'teste da inferência com o {active_adapter}, com scale {current_actived_adapter_scale}, resultado:{test_inference_with_adapter}')
-
-messages.pop()
-slm.reset()
-
-
-## logs do resultado
-
-# llama_context: n_ctx_seq (2048) < n_ctx_train (40960) -- the full capacity of the model will not be utilized
-# planner carregado com sucesso em memória.
-# <llama_cpp.llama_cpp.LP_c_void_p object at 0x77559a4e3cd0>
-# cópia do planner 1 carregado com sucesso em memória.
-# <llama_cpp.llama_cpp.LP_c_void_p object at 0x7755988308d0>
-# cópia do planner 2 carregado com sucesso em memória.
-# <llama_cpp.llama_cpp.LP_c_void_p object at 0x7755988318d0>
-# adapter ativo: adapter_a, scale: 0.5
-# teste da inferência com o <function active_adapter at 0x77559882e3e0>, com scale 0.5, resultado:{'id': 'chatcmpl-9dd50c29-712d-4b98-ba53-82fcdd0f6335', 'object': 'chat.completion', 'created': 1775774732, 'model': 'slm/Qwen3-0.6B-Q8_0.gguf', 'choices': [{'index': 0, 'message': {'role': 'assistant', 'content': '<think>\n\n</think>\n\nClaro! Aí está a chamada: **Call 10 horas**.'}, 'logprobs': None, 'finish_reason': 'stop'}], 'usage': {'prompt_tokens': 47, 'completion_tokens': 22, 'total_tokens': 69}}
-# adapter ativo: adapter_b, scale: 0.1
-# teste da inferência com o <function active_adapter at 0x77559882e3e0>, com scale 0.1, resultado:{'id': 'chatcmpl-7f1156f9-be2c-46a1-b7d9-a5beba17d449', 'object': 'chat.completion', 'created': 1775774738, 'model': 'slm/Qwen3-0.6B-Q8_0.gguf', 'choices': [{'index': 0, 'message': {'role': 'assistant', 'content': '<think>\n\n</think>\n\nCall dezenas.'}, 'logprobs': None, 'finish_reason': 'stop'}], 'usage': {'prompt_tokens': 47, 'completion_tokens': 9, 'total_tokens': 56}}
-# adapter ativo: adapter_c, scale: 1.0
-# teste da inferência com o <function active_adapter at 0x77559882e3e0>, com scale 1.0, resultado:{'id': 'chatcmpl-db389622-227a-4107-8c32-94d8cda7b936', 'object': 'chat.completion', 'created': 1775774742, 'model': 'slm/Qwen3-0.6B-Q8_0.gguf', 'choices': [{'index': 0, 'message': {'role': 'assistant', 'content': '<think>\n\n</think>\n\nNão, não faço isso. Posso ajudar com call ou notificações.'}, 'logprobs': None, 'finish_reason': 'stop'}], 'usage': {'prompt_tokens': 47, 'completion_tokens': 22, 'total_tokens': 69}}
-# adapter ativo: adapter_a, scale: 0.7
-# teste da inferência com o <function active_adapter at 0x77559882e3e0>, com scale 0.7, resultado:{'id': 'chatcmpl-404fdc48-46b9-435e-9447-4ccdb42b5709', 'object': 'chat.completion', 'created': 1775774748, 'model': 'slm/Qwen3-0.6B-Q8_0.gguf', 'choices': [{'index': 0, 'message': {'role': 'assistant', 'content': '<think>\n\n</think>\n\nCall de dez horas.'}, 'logprobs': None, 'finish_reason': 'stop'}], 'usage': {'prompt_tokens': 47, 'completion_tokens': 9, 'total_tokens': 56}}
-# adapter ativo: adapter_a, scale: 0.0
-# teste da inferência com o <function active_adapter at 0x77559882e3e0>, com scale 0.0, resultado:{'id': 'chatcmpl-3f83a9e8-8081-482b-adba-9be3e85dec4e', 'object': 'chat.completion', 'created': 1775774754, 'model': 'slm/Qwen3-0.6B-Q8_0.gguf', 'choices': [{'index': 0, 'message': {'role': 'assistant', 'content': '<think>\n\n</think>\n\nClaro! A marca da chamada é **"Call"**.'}, 'logprobs': None, 'finish_reason': 'stop'}], 'usage': {'prompt_tokens': 47, 'completion_tokens': 19, 'total_tokens': 66}}
-
-
-
-
-
-
-
  
+
+actived_adapter, current_actived_adapter_scale = active_adapter(slm.ctx, 'adapter_b', adapters_pointer_python_list, adapters_pointers_c_array, scales_c_float_array, personalized_scale=0.5)
+print(f'adapter ativo: {actived_adapter}, scale: {current_actived_adapter_scale}')
+
+kv_cache_adapters['adapter_b']['messages'].append(create_user_message('Qual a capital da Bahia?'))
+
+test_inference_with_adapter = slm.create_chat_completion(messages=kv_cache_adapters['adapter_a']['messages'], max_tokens=100, stream=False)
+
+print(f'teste da inferência com o {active_adapter}, com scale {current_actived_adapter_scale}, resultado:{test_inference_with_adapter}')
+
+kv_cache_adapters['adapter_b']['state'] = slm.save_state()
+
+print(kv_cache_adapters['adapter_b']['state'].n_tokens)
+
+slm.reset()
+
+
+print(kv_cache_adapters['adapter_a']['state'].n_tokens)
+slm.load_state(kv_cache_adapters['adapter_a']['state'])
+actived_adapter, current_actived_adapter_scale = active_adapter(slm.ctx, 'adapter_a', adapters_pointer_python_list, adapters_pointers_c_array, scales_c_float_array, personalized_scale=1.0)
+print(f'adapter ativo: {actived_adapter}, scale: {current_actived_adapter_scale}')
+
+kv_cache_adapters['adapter_a']['messages'].append(create_user_message('diga apenas exatamente o nome do estado e repita exatamente a minha última pergunta'))
+
+test_inference_with_adapter = slm.create_chat_completion(messages=kv_cache_adapters['adapter_a']['messages'], max_tokens=100, stream=False)
+
+print(f'teste da inferência com o {active_adapter}, com scale {current_actived_adapter_scale}, resultado:{test_inference_with_adapter}')
+
+kv_cache_adapters['adapter_a']['state'] = slm.save_state()
+
+
+print(kv_cache_adapters['adapter_a']['state'].n_tokens)
+
+slm.reset()
+
+
+
+def _load_skills():
+    pass
+
+
+def _load_learning():
+    pass
+
+def switch_agent(target_agent):
+
+
+    
+    _load_skills()
+    _load_learning()
+    
+    
+    pass
  
     
 
